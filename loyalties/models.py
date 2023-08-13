7dbb50ee4635a932e7a7259915466b466288b8de
sanya mahajan <sanyamahajan08@gmail.com>
2023-08-13 17:29:05 +0530

    default

--- loyalties/models.py
+++ loyalties/models.py
@@ -43,7 +43,7 @@ class TierProgression(models.Model):
 
 class LoyaltyProfile(models.Model):
     customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
-    loyalty_tier = models.ForeignKey(TierProgression, on_delete=models.PROTECT)
+    loyalty_tier = models.ForeignKey(TierProgression, on_delete=models.PROTECT,default=1)
     current_points=models.PositiveIntegerField( default=0)
     maximum_points=models.PositiveIntegerField(default=0)
 
